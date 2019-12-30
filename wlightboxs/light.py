3d71e2ac495ec08c6cfb2f33661aa0142a83fdb4
amnyu <natkins92@gmail.com>
2019-12-30 15:06:34 +0100

    Fixed imports

--- wlightboxs/light.py
+++ wlightboxs/light.py
@@ -1,13 +1,12 @@
 import logging
-import voluptuous as vol
-
-import requests
 from time import sleep
-# Import the device class from the component that you want to support
+
+import homeassistant.helpers.config_validation as cv
+import requests
+import voluptuous as vol
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS, Light, PLATFORM_SCHEMA, SUPPORT_BRIGHTNESS)
-from homeassistant.const import CONF_HOST
-import homeassistant.helpers.config_validation as cv
+from homeassistant.const import CONF_HOST, CONF_NAME
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -22,9 +21,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Setup the wLightBoxS platform."""
     host = config.get(CONF_HOST)
-    device_name = host.get(CONF_NAME)
+    device_name = config.get(CONF_NAME)
 
     device = 'http://{}/'.format(host)
     device_state = requests.get(device + "api/device/state")
@@ -40,11 +38,8 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
 
 class wLightBoxS(Light):
-    """Representation of an wLightBoxS."""
 
     def __init__(self, light, name=None):
-        """Initialize a wLightBoxS."""
-
         self._light = light
         self._name = name
         self._state = None
@@ -73,13 +68,12 @@ class wLightBoxS(Light):
         requests.get(self._light + "s/" + brigthness_hex)
 
     def turn_off(self, **kwargs):
-        if self._state:
-            requests.get(self._light + "s/00")
+        requests.get(self._light + "s/00")
 
     def update(self):
         light_state = requests.get(self._light + "api/light/state")
 
-        brightness_raw = light_state.json()['light']['currentColor']
+        brightness_raw = light_state.json()['light']['desiredColor']
         brightness = int(brightness_raw, 16)
 
         self._state = brightness > 0
